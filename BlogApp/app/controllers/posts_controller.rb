8319ba821bd5b412e0cf8804188a2cd7a900162f
Elena <elenaczubiak@gmail.com>
2015-11-13 01:57:37 +0000

    Created Blog app with pagination & comments

---
+++ BlogApp/app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+    
+    before_action :authenticate_user!
+    
+    def index
+
+        @posts = current_user.posts.paginate(page: params[:page], per_page: 3)
+    end
+    
+    def new
+        @user = current_user
+        @post = Post.new
+    end
+    
+    def create
+        current_user.posts.create(post_params)
+        
+        redirect_to user_posts_path
+    end
+    
+    def show
+        @post = Post.find(params[:id])
+        @user = User.find(params[:user_id])
+       
+    end    
+    
+private
+
+def post_params
+    params.require(:post).permit(:title, :content, :image)
+end    
+end
